fdfbe1b96bd3037c314035fb34d42b659cb79437
Josh White <jojawhi@gmail.com>
2022-02-05 16:27:33 -0800

    installed css loader and connect bundle.js and style.css to index.html

--- src/index.js
+++ src/index.js
@@ -1,1 +1,19 @@
+import './style.css';
+import Logo from './nanda-logo.jpg';
+
+const content = document.querySelector('#content');
+
+const logo = () => {
+    
+    const myLogo = new Image();
+    myLogo.src = Logo;
+    myLogo.classList.add('logo');
+
+    content.appendChild(myLogo);
+
+
+}
+
+logo();
+
 console.log('Hell low whirled!');
